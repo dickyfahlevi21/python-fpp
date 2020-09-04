d5683252fd638b332efe7ecf7df71bb2fdcf075d
dickyfahlevi21 <dicky.fahlevi21@gmail.com>
2020-09-05 00:40:13 +0700

    no.1

--- 01.py
+++ 01.py
@@ -1,5 +1,5 @@
-from numbers import *
-from grades import *
+numbers = [1,2,3,4,5,6,6,8,8,6,9]
+grades = [87.5, 88.5, 60.5, 90.5, 88.5]
 
 def Mode(value):
     return max(set(value), key = value.count)
